d2617330e6a2ba7f3e009b48b572dac5b10ea34b
srinivassistla16 <srini.sistla16@gmail.com>
2025-04-12 22:56:48 +0530

    Initial Version oauth2

---
+++ src/main/java/com/srinivas/oauth2/SpringBootSecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package com.srinivas.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityOauth2Application.class, args);
+	}
+
+}
